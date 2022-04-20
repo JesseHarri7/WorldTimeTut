c2c4bdddf624c69ec4d007b426329719489aede8
jesseh <jesse.hari@gmail.com>
2022-04-21 00:02:04 +0200

    First commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime worldTime = new WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await worldTime.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location' : worldTime.location,
+      'flag' : worldTime.flag,
+      'time' : worldTime.time,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Text('Loading'),
+      ),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,44 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+
+class WorldTime {
+
+  late String location; // location name for the UI
+  late String time; // time in that location
+  late String flag; // url to asset flag icon
+  late String url; // location url for api endpoint
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+
+    try {
+      //Make request
+      Response response = await get(
+          Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      //print(data);
+
+      //get properties from data
+      String dateTime = data['utc_datetime'];
+      String offset = data['utc_offset'];
+      offset = offset.substring(1, 3);
+      //print(dateTime);
+      //print(offset);
+
+      //create dataTime object
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      //print(now);
+
+      //set time property
+      time = DateFormat.jm().format(now);
+    }catch(e) {
+      print(e);
+      time = 'Could not get time data';
+    }
+  }
+
+}
